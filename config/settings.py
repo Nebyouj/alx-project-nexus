c789e0100a45607c85132c09df6db9694eed2d45
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-09-27 10:51:01 +0300

    project-nexus-version4

--- config/settings.py
+++ config/settings.py
@@ -178,13 +178,15 @@ DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@projectnexus.com")
 
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
-CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
+CELERY_BROKER_URL = "memory://"
 
-ssl_options = {
-    "ssl_cert_reqs": ssl.CERT_NONE,  
-    "ssl_ca_certs": os.environ.get("REDIS_SSL_CA_CERTS", "/etc/ssl/certs/ca-certificates.crt"),
-}
+# CELERY_BROKER_URL = os.environ.get("CELERY_BROKER_URL")
+# CELERY_RESULT_BACKEND = os.environ.get("CELERY_RESULT_BACKEND")
 
-CELERY_BROKER_TRANSPORT_OPTIONS = ssl_options
-CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS = ssl_options
+# ssl_options = {
+#     "ssl_cert_reqs": ssl.CERT_NONE,  
+#     "ssl_ca_certs": os.environ.get("REDIS_SSL_CA_CERTS", "/etc/ssl/certs/ca-certificates.crt"),
+# }
+
+# CELERY_BROKER_TRANSPORT_OPTIONS = ssl_options
+# CELERY_RESULT_BACKEND_TRANSPORT_OPTIONS = ssl_options
