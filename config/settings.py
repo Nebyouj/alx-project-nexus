fc5db1ced7d0734a8023ed31c362ab8b5c604504
Nebyou Damtew <nebyoudamtew18@gmail.com>
2025-09-27 12:16:04 +0300

    project-nexus-version4

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,17 @@ SIMPLE_JWT = {
 # EMAIL_USE_TLS = False
 # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@projectnexus.com")
+# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@projectnexus.com")
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = os.getenv("SENDGRID_HOST", "smtp.sendgrid.net")
+EMAIL_PORT = int(os.getenv("SENDGRID_PORT", 587))
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = os.getenv("SENDGRID_USERNAME", "apikey")  # literally "apikey"
+EMAIL_HOST_PASSWORD = os.getenv("SENDGRID_API_KEY")          # your SendGrid API key
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@yourdomain.com")
+
 
 # CELERY_BROKER_URL = 'redis://localhost:6379/0'
 
